ca9302f3742115b1b026b6e131cab4f716b44ea9
Ihor Petruniv <ipetruniv@gmail.com>
2019-03-18 18:31:02 +0200

    change path

--- test_res.bat
+++ test_res.bat
@@ -1,6 +1,4 @@
-qres.exe /l > "%USERPROFILE%\Desktop\Modes.txt"
-for /f "tokens=1-5 delims=x, " %%a in (%USERPROFILE%\Desktop\Modes.txt) do echo /X:%%a /Y:%%b /C:%%c > "%USERPROFILE%\Desktop\Resolution.txt"
-for /f "tokens=*" %%i in (%USERPROFILE%\Desktop\Resolution.txt) do qres.exe %%i
+qres.exe /l > Modes.txt"
+for /f "tokens=1-5 delims=x, " %%a in Modes.txt do echo /X:%%a /Y:%%b /C:%%c > Resolution.txt
+for /f "tokens=*" %%i in Resolution.txt do qres.exe %%i
 ping 127.0.0.1 -n 2 -w 1000 > nul
-rem del %USERPROFILE%\Desktop\Modes.txt
-rem del %USERPROFILE%\Desktop\Resolution.txt
